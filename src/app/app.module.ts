01f53510b081f4b20864f7998f58a07b0f143b54
franco <francogl91@hotmail.com>
2017-09-16 13:01:11 -0300

    1er_Actualizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,86 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
+import { BrowserModule }                            from '@angular/platform-browser';
+import { ErrorHandler, NgModule }                   from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen }                             from '@ionic-native/splash-screen';
+import { StatusBar }                                from '@ionic-native/status-bar';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+
+import { MyApp }                from './app.component';
+import { HomePage }             from '../pages/home/home';
+import { ProductosPage }        from '../pages/productos/productos';
+import { ClientesPage }         from '../pages/clientes/clientes';
+import { PedidosPage }          from '../pages/pedidos/pedidos';
+import { VendedoresPage }       from '../pages/vendedores/vendedores';
+import { SignInPage }           from '../pages/signin/signin';
+import { SignUpPage }           from '../pages/signup/signup';
+import {PerfilPage}             from '../pages/perfil/perfil';
+import {DetallePage}            from '../pages/detalle/detalle';
+import {RegistrarPage}          from '../pages/registrar/registrar';
+import {FormularioPage}          from '../pages/formulario/formulario';
+
+import { AngularFireModule }         from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule }     from 'angularfire2/auth';
+
+import { AuthService } from '../providers/auth-service';
+import { NotesService } from '../providers/notes.servise';
+
+export const firebaseConfig = {
+ apiKey: "AIzaSyCeLmkUIANCNvjQU0IRhiqjxYQKr6ds_UI",
+    authDomain: "apppfc.firebaseapp.com",
+    databaseURL: "https://apppfc.firebaseio.com",
+    projectId: "apppfc",
+    storageBucket: "apppfc.appspot.com",
+    messagingSenderId: "848922468545"
+};
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ProductosPage,
+    ClientesPage,
+    PedidosPage,
+    VendedoresPage,
+    SignInPage ,
+    SignUpPage,
+    PerfilPage,
+
+    DetallePage,
+    RegistrarPage,
+    FormularioPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ProductosPage,
+    ClientesPage,
+    PedidosPage,
+    PerfilPage,
+     VendedoresPage,
+    
+    SignInPage ,
+    SignUpPage,
+
+    DetallePage,
+    RegistrarPage,
+
+    FormularioPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler },
+    AuthService,
+    NotesService
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,64 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController, MenuController} from 'ionic-angular';
 
+import {AngularFireAuth} from 'angularfire2/auth';
+import {perfil} from '../../models/perfil'
+import { AngularFireDatabase, FirebaseObjectObservable, FirebaseListObservable } from 'angularfire2/database';
+import * as firebase from 'firebase/app';
+import { Observable } from "rxjs/Observable";
+import { SignInPage }           from '../../pages/signin/signin';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+ mi_perfil:any;
+  user: Observable<firebase.User>;
+  perfilDatos: FirebaseObjectObservable <any>;
+  productos: FirebaseListObservable<any>
+  constructor(private afAuth:AngularFireAuth,
+  public afDatabase: AngularFireDatabase,
+  private toast:ToastController,
+  public navCtrl:NavController,
+  public navParams:NavParams,
+  public menuCtrl: MenuController,
+ 
+) {
+const db = firebase.database();
+const alumnos= db.ref().child("Perfil");
 
-  constructor(public navCtrl: NavController) {
-
+  // this.listaPerfil().subscribe( perfil => {this.mi_perfil=perfil});
+  
+  this.productos = this.afDatabase.list('/Clientes');
+    this.menuCtrl.get().enable(true)
+    this.user = afAuth.authState;
+    
   }
 
+  cerrarSesion(){
+    this.afAuth.auth.signOut();
+    this.navCtrl.push(SignInPage)
+  }
+  listaPerfil(){
+    return this.perfilDatos=this.afDatabase.object('/Perfil');
+      }
+ionViewWilload() {
+  this.afAuth.authState.take(1).subscribe(data =>{
+    if(data && data.email && data.uid){
+      this.toast.create({
+        message:`Bienvenido a AppPedidos,${data.email}`,
+        duration:3000 
+      }).present();
+
+      this.perfilDatos = this.afDatabase.object(`Perfil/${data.uid}`)
+    }
+    else{
+      this.toast.create({
+        message:'No se han encontrado los detalles de autenticacion.',
+        duration:3000
+      }).present();
+    }
+  })
 }
+  }
+
